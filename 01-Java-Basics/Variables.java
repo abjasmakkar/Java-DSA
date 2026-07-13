41f5e83a488c2cf3090eef5d7556fbb6763ff43e
Abjas Makkar <abjasmakkar@gmail.com>
2026-07-13 14:36:07 +0530

    Reorganize project structure and add Variables lesson

---
+++ 01-Java-Basics/Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+    public static void main(String[] args){
+        int age = 19;
+        double height = 5.4;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Abjas";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
